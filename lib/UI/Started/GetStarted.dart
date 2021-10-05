b0136e5fa805bfd088fb3d658e90605fa3471b41
Munawar <munawartmk@gmail.com>
2021-10-06 01:21:48 +0700

    fitnes_app

---
+++ lib/UI/Started/GetStarted.dart
@@ -0,0 +1,13 @@
+import 'package:fitnes_app/Constant.dart';
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: kprimeriColor,
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:fitnes_app/UI/Started/GetStarted.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Fitnes',
+      home: GetStarted(),
+    );
+  }
+}
